98d4d9a9768adb9d0f94836dae02e426f90beafd
jhrudden <johnhenryrudden@gmail.com>
2024-05-17 21:04:48 -0500

    telescope reformat

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 local function findFilesInConfig()
-    local builtin = require('telescope.builtin')
-    builtin.find_files { cwd = vim.fn.stdpath "config" }
+	local builtin = require("telescope.builtin")
+	builtin.find_files({ cwd = vim.fn.stdpath("config") })
 end
 
 local keys = {
@@ -11,48 +11,49 @@ local keys = {
 	{ "<leader>fj", "<cmd>Telescope command_history<cr>", desc = "History" },
 	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find Word (Current Working Dir" },
 	{ "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "Find Word Under Cursor" },
-    {"<leader>mc", findFilesInConfig, desc = "Search Files Nvim Config"},
+	{ "<leader>mc", findFilesInConfig, desc = "Search Files Nvim Config" },
 }
 
 local config = function()
-    local telescope = require('telescope')
-    telescope.setup({
-        defaults = {
-            mappings = {
-                i = {
-                    ["<C-j>"] = "move_selection_next",
-                    ["<C-k>"] = "move_selection_previous",
-                },
-                n = {
-                    ["q"] = "close",
-                },
-            },
-        },
-        pickers = {
-            find_files = {
-                theme = "dropdown",
-                previewer = false,
-                hidden = true,
-            },
-        },
-        extensions = {
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-j>"] = "move_selection_next",
+					["<C-k>"] = "move_selection_previous",
+				},
+				n = {
+					["q"] = "close",
+				},
+			},
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown",
+				previewer = false,
+				hidden = true,
+				file_ignore_patterns = { ".git" },
+			},
+		},
+		extensions = {
 			"fzf",
 		},
-    })
-    telescope.load_extension("fzf")
+	})
+	telescope.load_extension("fzf")
 end
 
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.6',
-    lazy = true,
-    dependencies = { 
-        {'nvim-lua/plenary.nvim'},
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	lazy = true,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
 		},
-    },
-    config = config,
-    keys = keys
+	},
+	config = config,
+	keys = keys,
 }
