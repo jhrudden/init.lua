1d75fc6286b3833287f4f8081deea29995cc90c9
jhrudden <johnhenryrudden@gmail.com>
2024-05-18 11:47:55 -0500

    add oil for file system augmentation (CRUD)

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		config = function()
+			local oil = require("oil")
+			oil.setup({
+				columns = { "icon" },
+				keymap = {
+					["<C-h>"] = false,
+				},
+				delete_to_trash = true,
+				view_options = {
+					show_hidden = false,
+				},
+			})
+			vim.keymap.set("n", "<leader>-", oil.toggle_float, { desc = "Open parent dir in floating window" })
+		end,
+	},
+}
